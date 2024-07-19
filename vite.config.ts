c60e5aa223bcad0edf89bd1f556b758ea8a2a317
mohanraj0410 <mohanrajs0410@gmail.com>
2024-07-19 18:40:59 +0530

    deploy commit

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/Social_Media_App_React/",
   server: {
     open: true,
     port: 3000,
