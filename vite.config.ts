dc845ff9da153463de786e8e3db52036d31ab366
mohanraj0410 <mohanrajs0410@gmail.com>
2024-12-07 17:36:22 +0530

    issue fix

--- vite.config.ts
+++ vite.config.ts
@@ -5,11 +5,19 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Social_Media_App_React/login",
+  
+  // Adjust base for GitHub Pages deployment
+  base: "/Social_Media_App_React/login",  // Set the base path to the repository name
+
+  build: {
+    chunkSizeWarningLimit: 1000, // Increase chunk size warning limit to 1 MB
+  },
+
   server: {
     open: true,
     port: 3000,
   },
+
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
